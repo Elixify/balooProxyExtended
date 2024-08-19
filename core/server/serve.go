e2d70c871acbef0e54e62d87ebdf289c9c71185b
H1v9 <davide.abbrescia@inventati.org>
2024-08-20 00:41:34 +0300

    Stealth mode touch-up

--- core/server/serve.go
+++ core/server/serve.go
@@ -86,7 +86,11 @@ func Serve() {
 
 			if !domainFound {
 				w.Header().Set("Content-Type", "text/plain")
-				fmt.Fprintf(w, "balooProxy: "+r.Host+" does not exist. If you are the owner please check your config.json if you believe this is a mistake")
+				name := ""
+				if proxy.Stealth {
+					name = "balooProxy: "
+				}
+				fmt.Fprintf(w, name+r.Host+" does not exist. If you are the owner please check your config.json if you believe this is a mistake")
 				return
 			}
 
